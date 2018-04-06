9ed8d8c817c08aaf9fbdb2860c33f1550aeb31ae
Thomas Field <tfield@evo.com>
2018-04-06 11:40:10 -0700

    Fix example program, allow serializing Order direction

--- GremlinSample/Program.cs
+++ GremlinSample/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Gremlin.Net.CosmosDb;
+using Gremlin.Net.Process.Traversal;
 using GremlinSample.Schema;
 using Newtonsoft.Json;
 using System;
@@ -8,13 +9,22 @@ namespace GremlinSample
 {
     internal class Program
     {
-        private static async Task Main(string[] args)
+        private static void Main(string[] args)
+        {
+            Run().Wait();
+
+            Console.WriteLine();
+            Console.WriteLine("All done...");
+            Console.ReadKey();
+        }
+
+        private static async Task Run()
         {
             using (var graphClient = new GraphClient("graph-test-bplehal.gremlin.cosmosdb.azure.com", "evo", "CMS", "6agjPRz3wZlwdErfasKon2kbNug0z75fVdfL53uuaG7OTZxOxqoyvId3irOCDJgfkgxUwlZ3qV5aNLLBSGWy1w=="))
             {
                 var g = graphClient.CreateTraversalSource();
 
-                var query = g.V<PersonVertex>("1").Out(v => v.Purchased);
+                var query = g.V<PersonVertex>("1").Out(v => v.Purchased).Order().By("id", Order.Decr);
                 Console.WriteLine(query.ToGremlinQuery());
                 var response = await graphClient.SubmitAsync(query);
 
@@ -26,10 +36,6 @@ namespace GremlinSample
                     Console.WriteLine(json);
                 }
             }
-
-            Console.WriteLine();
-            Console.WriteLine("All done...");
-            Console.ReadKey();
         }
     }
 }
