e1ad2db2e510c12d7a3a9d657fa0458dec457e85
Michael Gearhardt <michael.gearhardt@fathym.com>
2019-01-25 08:44:02 -0700

    Adjusted to query

--- src/Gremlin.Net.CosmosDb/Serialization/GremlinQuerySerializer.cs
+++ src/Gremlin.Net.CosmosDb/Serialization/GremlinQuerySerializer.cs
@@ -10,247 +10,250 @@ using System.Linq;
 
 namespace Gremlin.Net.CosmosDb.Serialization
 {
-    /// <summary>
-    /// Helper gremlin query serializer for <see cref="Gremlin.Net.Process.Traversal.ITraversal"/> objects
-    /// </summary>
-    /// <seealso cref="System.IDisposable"/>
-    public class GremlinQuerySerializer : IDisposable
-    {
-        private static readonly JsonSerializerSettings DEFAULT_SERIALIZER_SETTINGS = new JsonSerializerSettings
-        {
-            Converters = new JsonConverter[]
-            {
-                new IsoDateTimeConverter
-                {
-                    DateTimeStyles = DateTimeStyles.AssumeUniversal
-                }
-            },
-            DateFormatHandling = DateFormatHandling.IsoDateFormat,
-            DateParseHandling = DateParseHandling.DateTimeOffset,
-            DateTimeZoneHandling = DateTimeZoneHandling.Utc
-        };
+	/// <summary>
+	/// Helper gremlin query serializer for <see cref="Gremlin.Net.Process.Traversal.ITraversal"/> objects
+	/// </summary>
+	/// <seealso cref="System.IDisposable"/>
+	public class GremlinQuerySerializer : IDisposable
+	{
+		private static readonly JsonSerializerSettings DEFAULT_SERIALIZER_SETTINGS = new JsonSerializerSettings
+		{
+			Converters = new JsonConverter[]
+			{
+				new IsoDateTimeConverter
+				{
+					DateTimeStyles = DateTimeStyles.AssumeUniversal
+				}
+			},
+			DateFormatHandling = DateFormatHandling.IsoDateFormat,
+			DateParseHandling = DateParseHandling.DateTimeOffset,
+			DateTimeZoneHandling = DateTimeZoneHandling.Utc
+		};
 
-        private readonly JsonSerializerSettings _serializerSettings;
-        private readonly TextWriter _writer;
+		private readonly JsonSerializerSettings _serializerSettings;
+		private readonly TextWriter _writer;
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="GremlinQuerySerializer"/> class.
-        /// </summary>
-        /// <param name="writer">The writer.</param>
-        /// <exception cref="ArgumentNullException">writer</exception>
-        public GremlinQuerySerializer(TextWriter writer)
-        {
-            _serializerSettings = DEFAULT_SERIALIZER_SETTINGS;
-            _writer = writer ?? throw new ArgumentNullException(nameof(writer));
-        }
+		/// <summary>
+		/// Initializes a new instance of the <see cref="GremlinQuerySerializer"/> class.
+		/// </summary>
+		/// <param name="writer">The writer.</param>
+		/// <exception cref="ArgumentNullException">writer</exception>
+		public GremlinQuerySerializer(TextWriter writer)
+		{
+			_serializerSettings = DEFAULT_SERIALIZER_SETTINGS;
+			_writer = writer ?? throw new ArgumentNullException(nameof(writer));
+		}
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="GremlinQuerySerializer"/> class.
-        /// </summary>
-        /// <param name="writer">The writer.</param>
-        /// <param name="serializerSettings">The serializer settings.</param>
-        /// <exception cref="ArgumentNullException">writer</exception>
-        public GremlinQuerySerializer(TextWriter writer, JsonSerializerSettings serializerSettings)
-        {
-            _serializerSettings = serializerSettings ?? throw new ArgumentNullException(nameof(serializerSettings));
-            _writer = writer ?? throw new ArgumentNullException(nameof(writer));
-        }
+		/// <summary>
+		/// Initializes a new instance of the <see cref="GremlinQuerySerializer"/> class.
+		/// </summary>
+		/// <param name="writer">The writer.</param>
+		/// <param name="serializerSettings">The serializer settings.</param>
+		/// <exception cref="ArgumentNullException">writer</exception>
+		public GremlinQuerySerializer(TextWriter writer, JsonSerializerSettings serializerSettings)
+		{
+			_serializerSettings = serializerSettings ?? throw new ArgumentNullException(nameof(serializerSettings));
+			_writer = writer ?? throw new ArgumentNullException(nameof(writer));
+		}
 
-        /// <summary>
-        /// Serializes the specified traversal.
-        /// </summary>
-        /// <param name="traversal">The traversal.</param>
-        /// <exception cref="ArgumentNullException">traversal</exception>
-        public void Serialize(ITraversal traversal)
-        {
-            if (traversal == null)
-                throw new ArgumentNullException(nameof(traversal));
+		/// <summary>
+		/// Serializes the specified traversal.
+		/// </summary>
+		/// <param name="traversal">The traversal.</param>
+		/// <exception cref="ArgumentNullException">traversal</exception>
+		public void Serialize(ITraversal traversal)
+		{
+			if (traversal == null)
+				throw new ArgumentNullException(nameof(traversal));
 
-            Serialize(traversal.Bytecode);
-        }
+			Serialize(traversal.Bytecode);
+		}
 
-        /// <summary>
-        /// Serializes the specified bytecode.
-        /// </summary>
-        /// <param name="bytecode">The bytecode.</param>
-        private void Serialize(Bytecode bytecode)
-        {
-            var first = true;
-            foreach (var instr in bytecode.SourceInstructions)
-            {
-                if (!first)
-                    _writer.Write('.');
+		/// <summary>
+		/// Serializes the specified bytecode.
+		/// </summary>
+		/// <param name="bytecode">The bytecode.</param>
+		private void Serialize(Bytecode bytecode)
+		{
+			var first = true;
+			foreach (var instr in bytecode.SourceInstructions)
+			{
+				if (!first)
+					_writer.Write('.');
 
-                _writer.Write(instr.OperatorName);
+				_writer.Write(instr.OperatorName);
 
-                first = false;
-            }
+				first = false;
+			}
 
-            foreach (var instr in bytecode.StepInstructions)
-            {
-                if (!first)
-                    _writer.Write('.');
+			foreach (var instr in bytecode.StepInstructions)
+			{
+				if (!first)
+					_writer.Write('.');
 
-                Serialize(instr);
+				Serialize(instr);
 
-                first = false;
-            }
-        }
+				first = false;
+			}
+		}
 
-        /// <summary>
-        /// Serializes the specified cardinality.
-        /// </summary>
-        /// <param name="cardinality">The cardinality.</param>
-        private void Serialize(Cardinality cardinality)
-        {
-            _writer.Write(cardinality.EnumValue);
-        }
+		/// <summary>
+		/// Serializes the specified cardinality.
+		/// </summary>
+		/// <param name="cardinality">The cardinality.</param>
+		private void Serialize(Cardinality cardinality)
+		{
+			_writer.Write(cardinality.EnumValue);
+		}
 
-        /// <summary>
-        /// Serializes the specified Column enum.
-        /// </summary>
-        /// <param name="column">The column.</param>
-        private void Serialize(Column column)
-        {
-            _writer.Write(column.EnumValue);
-        }
+		/// <summary>
+		/// Serializes the specified Column enum.
+		/// </summary>
+		/// <param name="column">The column.</param>
+		private void Serialize(Column column)
+		{
+			_writer.Write(column.EnumValue);
+		}
 
-        /// <summary>
-        /// Serializes the specified Pop enum.
-        /// </summary>
-        /// <param name="pop">The pop.</param>
-        private void Serialize(Pop pop)
-        {
-            _writer.Write(pop.EnumValue);
-        }
+		/// <summary>
+		/// Serializes the specified Pop enum.
+		/// </summary>
+		/// <param name="pop">The pop.</param>
+		private void Serialize(Pop pop)
+		{
+			_writer.Write(pop.EnumValue);
+		}
 
-        /// <summary>
-        /// Serializes the specified unique identifier.
-        /// </summary>
-        /// <param name="guid">The unique identifier.</param>
-        private void Serialize(Guid guid)
-        {
-            Serialize(guid.ToString().ToLowerInvariant());
-        }
+		/// <summary>
+		/// Serializes the specified unique identifier.
+		/// </summary>
+		/// <param name="guid">The unique identifier.</param>
+		private void Serialize(Guid guid)
+		{
+			Serialize(guid.ToString().ToLowerInvariant());
+		}
 
-        /// <summary>
-        /// Serializes the specified instruction.
-        /// </summary>
-        /// <param name="instruction">The instruction.</param>
-        private void Serialize(Instruction instruction)
-        {
-            _writer.Write(instruction.OperatorName);
-            _writer.Write('(');
-            SerializeListWithCommas(instruction.Arguments);
-            _writer.Write(')');
-        }
+		/// <summary>
+		/// Serializes the specified instruction.
+		/// </summary>
+		/// <param name="instruction">The instruction.</param>
+		private void Serialize(Instruction instruction)
+		{
+			if (instruction.OperatorName == "V")
+				_writer.Write("g.");
 
-        /// <summary>
-        /// Serializes the specified order.
-        /// </summary>
-        /// <param name="order">The order.</param>
-        private void Serialize(Order order)
-        {
-            _writer.Write(order.EnumValue.ToString().ToLowerInvariant());
-        }
+			_writer.Write(instruction.OperatorName);
+			_writer.Write('(');
+			SerializeListWithCommas(instruction.Arguments);
+			_writer.Write(')');
+		}
 
-        /// <summary>
-        /// Serializes the specified predicate.
-        /// </summary>
-        /// <param name="predicate">The predicate.</param>
-        private void Serialize(P predicate)
-        {
-            _writer.Write(predicate.OperatorName);
-            _writer.Write('(');
-            if (predicate.Value != null)
-            {
-                var predicateValues = predicate.Value as IEnumerable<dynamic>;
-                if (predicateValues?.Any() ?? false)
-                    SerializeListWithCommas(predicateValues);
-                else
-                    Serialize(predicate.Value);
-            }
-            _writer.Write(')');
-        }
+		/// <summary>
+		/// Serializes the specified order.
+		/// </summary>
+		/// <param name="order">The order.</param>
+		private void Serialize(Order order)
+		{
+			_writer.Write(order.EnumValue.ToString().ToLowerInvariant());
+		}
 
-        /// <summary>
-        /// Serializes the specified object.
-        /// </summary>
-        /// <param name="obj">The object.</param>
-        private void Serialize(object obj)
-        {
-            var serializedObj = JsonConvert.SerializeObject(obj, Formatting.None, _serializerSettings);
+		/// <summary>
+		/// Serializes the specified predicate.
+		/// </summary>
+		/// <param name="predicate">The predicate.</param>
+		private void Serialize(P predicate)
+		{
+			_writer.Write(predicate.OperatorName);
+			_writer.Write('(');
+			if (predicate.Value != null)
+			{
+				var predicateValues = predicate.Value as IEnumerable<dynamic>;
+				if (predicateValues?.Any() ?? false)
+					SerializeListWithCommas(predicateValues);
+				else
+					Serialize(predicate.Value);
+			}
+			_writer.Write(')');
+		}
 
-            //make sure objects and arrays are serialized as string values
-            //double-escape double quoted values that could be serialized inside the object as property keys/values
-            if (serializedObj.Length > 0 && (serializedObj[0] == '{' || serializedObj[0] == '['))
-                serializedObj = '"' + serializedObj.Replace("\"", "\\\"").Replace("\\\\\"", "\\\\\\\"") + '"';
+		/// <summary>
+		/// Serializes the specified object.
+		/// </summary>
+		/// <param name="obj">The object.</param>
+		private void Serialize(object obj)
+		{
+			var serializedObj = JsonConvert.SerializeObject(obj, Formatting.None, _serializerSettings);
 
-            //dollar signs are escaped to avoid parsing errors on Cosmos'
-            //end since they are used for interpolation in groovy strings
-            serializedObj = serializedObj.Replace("$", "\\$");
+			//make sure objects and arrays are serialized as string values
+			//double-escape double quoted values that could be serialized inside the object as property keys/values
+			if (serializedObj.Length > 0 && (serializedObj[0] == '{' || serializedObj[0] == '['))
+				serializedObj = '"' + serializedObj.Replace("\"", "\\\"").Replace("\\\\\"", "\\\\\\\"") + '"';
 
-            _writer.Write(serializedObj);
-        }
+			//dollar signs are escaped to avoid parsing errors on Cosmos'
+			//end since they are used for interpolation in groovy strings
+			serializedObj = serializedObj.Replace("$", "\\$");
 
-        /// <summary>
-        /// Serializes the specified string.
-        /// </summary>
-        /// <param name="str">The string.</param>
-        private void Serialize(GroovyString str)
-        {
-            _writer.Write(str?.Value);
-        }
+			_writer.Write(serializedObj);
+		}
 
-        /// <summary>
-        /// Serializes the specified <see cref="PartitionKeyIdPair"/>
-        /// </summary>
-        /// <param name="pair"></param>
-        private void Serialize(PartitionKeyIdPair pair)
-        {
-            _writer.Write($"[\"{pair.PartitionKey}\", \"{pair.Id}\"]");
-        }
+		/// <summary>
+		/// Serializes the specified string.
+		/// </summary>
+		/// <param name="str">The string.</param>
+		private void Serialize(GroovyString str)
+		{
+			_writer.Write(str?.Value);
+		}
 
-        /// <summary>
-        /// Helper method that serializes a list of things with comma separators.
-        /// </summary>
-        /// <param name="list">The list.</param>
-        private void SerializeListWithCommas(IEnumerable<dynamic> list)
-        {
-            var addComma = false;
-            foreach (var value in list)
-            {
-                if (addComma)
-                    _writer.Write(',');
+		/// <summary>
+		/// Serializes the specified <see cref="PartitionKeyIdPair"/>
+		/// </summary>
+		/// <param name="pair"></param>
+		private void Serialize(PartitionKeyIdPair pair)
+		{
+			_writer.Write($"[\"{pair.PartitionKey}\", \"{pair.Id}\"]");
+		}
 
-                Serialize(value);
+		/// <summary>
+		/// Helper method that serializes a list of things with comma separators.
+		/// </summary>
+		/// <param name="list">The list.</param>
+		private void SerializeListWithCommas(IEnumerable<dynamic> list)
+		{
+			var addComma = false;
+			foreach (var value in list)
+			{
+				if (addComma)
+					_writer.Write(',');
 
-                addComma = true;
-            }
-        }
+				Serialize(value);
 
-        #region IDisposable Support
+				addComma = true;
+			}
+		}
 
-        private bool disposedValue = false; // To detect redundant calls
+		#region IDisposable Support
 
-        public void Dispose()
-        {
-            Dispose(true);
-        }
+		private bool disposedValue = false; // To detect redundant calls
 
-        private void Dispose(bool disposing)
-        {
-            if (!disposedValue)
-            {
-                disposedValue = true;
+		public void Dispose()
+		{
+			Dispose(true);
+		}
 
-                if (disposing)
-                {
-                    _writer.Dispose();
-                }
-            }
-        }
+		private void Dispose(bool disposing)
+		{
+			if (!disposedValue)
+			{
+				disposedValue = true;
 
-        #endregion IDisposable Support
-    }
+				if (disposing)
+				{
+					_writer.Dispose();
+				}
+			}
+		}
+
+		#endregion IDisposable Support
+	}
 }
---
+++ test/Gremlin.Net.CosmosDb.Tests/Serialization/GremlinQuerySerializerTests.cs
@@ -0,0 +1,55 @@
+﻿using FluentAssertions;
+using Gremlin.Net.Driver;
+using Gremlin.Net.Driver.Remote;
+using Gremlin.Net.Process.Traversal;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System.Globalization;
+using Xunit;
+
+namespace Gremlin.Net.CosmosDb.Serialization
+{
+	public class GremlinQuerySerializerTests
+	{
+		private static readonly JsonSerializerSettings settings = new JsonSerializerSettings
+		{
+			Converters = new JsonConverter[]
+				{
+					new TreeJsonConverter(),
+					new IEdgeJsonConverter(),
+					new ElementJsonConverter(),
+					new IVertexJsonConverter(),
+					new IsoDateTimeConverter
+					{
+						DateTimeStyles = DateTimeStyles.AdjustToUniversal
+					}
+				},
+			DateFormatHandling = DateFormatHandling.IsoDateFormat,
+			DateParseHandling = DateParseHandling.DateTimeOffset,
+			DateTimeZoneHandling = DateTimeZoneHandling.Utc
+		};
+
+		[Fact]
+		private void GremlinQuerySerializer()
+		{
+			//	TODO: Need to mock
+			var server = new GremlinServer();
+
+			var client = new GremlinClient(server);
+
+			var g = AnonymousTraversalSource.Traversal().WithRemote(new DriverRemoteConnection(client));
+
+			var entQuery = g.V().HasLabel("Enterprise")
+				.Has("Registry", "xxx")
+				.Has("PrimaryAPIKey", "XXX");
+
+			var appQuery = g.V(("aaa", "AAA"))
+				.HasLabel("Application")
+				.Has("Registry", "xxx2");
+
+			var query = g.V(entQuery).AddE("Owns").To(appQuery).ToGremlinQuery();
+
+			query.Should().Be("g.V(g.V().hasLabel(\"Enterprise\").has(\"Registry\",\"xxx\").has(\"PrimaryAPIKey\",\"XXX\")).addE(\"Owns\").to(g.V([\"aaa\", \"AAA\"]).hasLabel(\"Application\").has(\"Registry\",\"xxx2\"))");
+		}
+	}
+}
